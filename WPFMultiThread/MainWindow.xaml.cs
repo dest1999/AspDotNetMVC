3756dbd63eef0de14be0333b54ae4f30c5cd22cd
Oleg <serv_all@mail.ru>
2022-03-24 05:25:16 +0300

    Создана форма с текстовым полем, которое заполняется данными из другого потока

---
+++ WPFMultiThread/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace WPFMultiThread
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool interruptToken = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Thread fibThread = new(FibonacciNumberAdd);
+            
+            fibThread.Start();
+
+        }
+
+
+        private void FibonacciNumberAdd()
+        {
+            int a = 0;
+            int b = 1;
+            while (!interruptToken)
+            {
+                string tmpStr = FibonacciCalculate(ref a, ref b).ToString();
+                textBlock.Dispatcher.BeginInvoke(DispatcherPriority.Background , new Action(() =>
+                {
+                    textBlock.Inlines.Add(tmpStr + "\n");
+                }));
+            }
+        }
+
+        private int FibonacciCalculate(ref int a, ref int b)
+        {
+            int currentFibNumber = a + b;
+            a = b;
+            b = currentFibNumber;
+            return currentFibNumber;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            interruptToken = true;
+        }
+    }
+}
