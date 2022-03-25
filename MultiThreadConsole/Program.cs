50ba9f1857e150d34125e1bbcc90c4b7cbb9cefe
Oleg <serv_all@mail.ru>
2022-03-25 22:58:26 +0300

    Ошибка при отмене потока не возникает

--- MultiThreadConsole/Program.cs
+++ MultiThreadConsole/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Thread thread1 = new(Method);
+thread1.Name = "MyThread";
+thread1.Start();
+
+Console.WriteLine($"New thread id: {thread1.ManagedThreadId}");
+
+thread1.Join();
+thread1.Interrupt();
+
+
+static void Method()
+{
+    int i = 0;
+    while (i < 500)
+    {
+        i++;
+        Console.WriteLine(i);
+        Thread.Sleep(10);
+    }
+}
