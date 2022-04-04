8fe262a3f19063b4b7dab5b961a8084d7dd45d5a
Oleg <serv_all@mail.ru>
2022-04-04 18:12:51 +0300

    ICommand WPF Начало

---
+++ ICommandPattern/Entityes/Storage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ICommandPattern
+{
+    internal static class Storage
+    {
+        private static List<ICommand> list = new();
+
+        public static void Add(ICommand item)
+        {
+            list.Add(item);
+        }
+
+        public static ICommand? GetById(int id)
+        {
+            if(id < list.Count)
+            {
+                return list[id];
+            }
+            return null;
+        }
+
+    }
+}
