235e3fe7542ecd29969980b280fb508265c1705d
Oleg <serv_all@mail.ru>
2022-04-13 00:29:58 +0300

    Creation ThreadPool in progress

---
+++ ThreadComsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using ThreadComsole;
+
+
+
+var thPool = ThreadsPool.GetInstance();
+
+
+for (int i = 0; i < 7; i++)
+{
+    var tmp = i;
+    var thread = new Thread(() => TestMethod(tmp));
+    thPool.AddNewThread(thread);
+
+}
+
+
+static void TestMethod(int i)
+{
+    //Thread.Sleep(1000);
+    Console.WriteLine(i);
+
+}
+
+
+
---
+++ ThreadComsole/ThreadsPool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadComsole
+{
+    internal class ThreadsPool
+    {
+        private static Thread[] threads = new Thread[4];
+        private Queue<Thread> queue = new ();
+
+        private static ThreadsPool? threadPool = null;
+
+        private ThreadsPool()
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static ThreadsPool GetInstance()
+        {
+            if (threadPool == null)
+            {
+                threadPool = new ThreadsPool();
+                return threadPool;
+            }
+            return threadPool;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void AddNewThread(Thread thread)
+        {
+            queue.Enqueue(thread);
+            for (int i = 0; i < threads.Length; i++)
+            {
+                if (threads[i] == null || !threads[i].IsAlive)
+                {
+                    threads[i] = queue.Dequeue();
+                    threads[i].Start();
+                    break;
+                }
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
