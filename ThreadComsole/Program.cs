66cae680e7fd4f86f5d10c7a8bbdb687e2fd4747
Oleg <serv_all@mail.ru>
2022-04-13 02:39:25 +0300

    Выполнено, рефакторинг

--- ThreadComsole/Program.cs
+++ ThreadComsole/Program.cs
@@ -1,25 +1,18 @@
-﻿using ThreadComsole;
-
-
+﻿using ThreadConsole;
 
 var thPool = ThreadsPool.GetInstance();
+Random rnd = new();
 
-
-for (int i = 0; i < 7; i++)
+for (int i = 0; i < 15; i++)
 {
     var tmp = i;
-    var thread = new Thread(() => TestMethod(tmp));
+    var thread = new Thread(() => TestMethod(tmp, rnd));
     thPool.AddNewThread(thread);
-
 }
 
 
-static void TestMethod(int i)
+static void TestMethod(int i, Random rnd)
 {
-    //Thread.Sleep(1000);
+    Thread.Sleep(rnd.Next(100, 500));
     Console.WriteLine(i);
-
 }
-
-
-
--- ThreadComsole/ThreadsPool.cs
+++ ThreadComsole/ThreadsPool.cs
@@ -1,38 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
-namespace ThreadComsole
+namespace ThreadConsole;
+
+internal class ThreadsPool
 {
-    internal class ThreadsPool
+    private readonly Thread[] threads = new Thread[8];
+    private readonly Queue<Thread> queue = new ();
+
+    private static ThreadsPool? threadPool = null;
+    private bool needToCheckEmptyTask = false;
+
+    private ThreadsPool()
     {
-        private static Thread[] threads = new Thread[4];
-        private Queue<Thread> queue = new ();
+    }
 
-        private static ThreadsPool? threadPool = null;
-
-        private ThreadsPool()
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    public static ThreadsPool GetInstance()
+    {
+        if (threadPool == null)
         {
-        }
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public static ThreadsPool GetInstance()
-        {
-            if (threadPool == null)
-            {
-                threadPool = new ThreadsPool();
-                return threadPool;
-            }
+            threadPool = new ThreadsPool();
             return threadPool;
         }
+        return threadPool;
+    }
 
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public void AddNewThread(Thread thread)
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    public void AddNewThread(Thread thread)
+    {
+        queue.Enqueue(thread);
+
+        do
         {
-            queue.Enqueue(thread);
             for (int i = 0; i < threads.Length; i++)
             {
                 if (threads[i] == null || !threads[i].IsAlive)
@@ -42,13 +41,15 @@ namespace ThreadComsole
                     break;
                 }
             }
-
-        }
-
-
-
-
-
-
+            if (queue.Count == 0)
+            {
+                needToCheckEmptyTask = false;
+            }
+            else
+            {
+                needToCheckEmptyTask = true;
+                Thread.Sleep(50);
+            }
+        } while (needToCheckEmptyTask);
     }
 }
